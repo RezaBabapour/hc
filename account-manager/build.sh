5269a2bc2ac7d6304c1e19f0e2e40d0078af1415
reza babapour <reza.babapour91@gmail.com>
2023-02-05 13:13:44 -0500

    Add dockerfile for services

---
+++ account-manager/build.sh
@@ -0,0 +1,2 @@
+#!/bin/bash
+docker build -t "rezababapour/hc-account" . ; docker push rezababapour/hc-account
---
+++ auth/build.sh
@@ -0,0 +1,2 @@
+#!/bin/bash
+docker build -t "rezababapour/hc-auth" . ; docker push rezababapour/hc-auth
